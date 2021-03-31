0f3d264e1981015c75ac9a8d984edd045ff515a8
Silas Leme Silvério <comikpro22@gmail.com>
2021-03-31 12:16:01 -0300

    DataReductiong funcional

---
+++ 1-Preprocessing/DataReduction.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+
+
+def main():
+    # Faz a leitura do arquivo
+    input_file = '0-Datasets/hcc-dataClear.txt'
+    names = ['Gender', 'Symptoms', 'Alcohol', 'Hepatitis B Surface Antigen', 'Hepatitis B e Antigen', 'Hepatitis B Core Antibody', 'Hepatitis C Virus Antibody', 'Cirrhosis', 'Endemic Countries', 'Smoking', 'Diabetes', 'Obesity', 'Hemochromatosis', 'Arterial Hypertension', 'Chronic Renal Insufficiency', 'Human Immunodeficiency Virus', 'Nonalcoholic Steatohepatitis', 'Esophageal Varices', 'Splenomegaly', 'Portal Hypertension', 'Portal Vein Thrombosis', 'Liver Metastasis', 'Radiological Hallmark', 'Age at diagnosis', 'Grams of Alcohol per day', 'Packs of cigarets per year',
+             'Performance Status', 'Encefalopathy degree', 'Ascites degree', 'International Normalised Ratio', 'Alpha-Fetoprotein (ng/mL)', 'Haemoglobin (g/dL)', 'Mean Corpuscular Volume (fl)', 'Leukocytes(G/L)', 'Platelets (G/L)', 'Albumin (mg/dL)', 'Total Bilirubin(mg/dL)', 'Alanine transaminase (U/L)', 'Aspartate transaminase (U/L)', 'Gamma glutamyl transferase (U/L)', 'Alkaline phosphatase (U/L)', 'Total Proteins (g/dL)', 'Creatinine (mg/dL)', 'Number of Nodules', 'Major dimension of nodule (cm)', 'Direct Bilirubin (mg/dL)', 'Iron (mcg/dL)', 'Oxygen Saturation (%)', 'Ferritin (ng/mL)', 'Class']
+    features = ['Gender', 'Symptoms', 'Alcohol', 'Hepatitis B Surface Antigen', 'Hepatitis B e Antigen', 'Hepatitis B Core Antibody', 'Hepatitis C Virus Antibody', 'Cirrhosis', 'Endemic Countries', 'Smoking', 'Diabetes', 'Obesity', 'Hemochromatosis', 'Arterial Hypertension', 'Chronic Renal Insufficiency', 'Human Immunodeficiency Virus', 'Nonalcoholic Steatohepatitis', 'Esophageal Varices', 'Splenomegaly', 'Portal Hypertension', 'Portal Vein Thrombosis', 'Liver Metastasis', 'Radiological Hallmark', 'Age at diagnosis', 'Grams of Alcohol per day', 'Packs of cigarets per year',
+                'Performance Status', 'Encefalopathy degree', 'Ascites degree', 'International Normalised Ratio', 'Alpha-Fetoprotein (ng/mL)', 'Haemoglobin (g/dL)', 'Mean Corpuscular Volume (fl)', 'Leukocytes(G/L)', 'Platelets (G/L)', 'Albumin (mg/dL)', 'Total Bilirubin(mg/dL)', 'Alanine transaminase (U/L)', 'Aspartate transaminase (U/L)', 'Gamma glutamyl transferase (U/L)', 'Alkaline phosphatase (U/L)', 'Total Proteins (g/dL)', 'Creatinine (mg/dL)', 'Number of Nodules', 'Major dimension of nodule (cm)', 'Direct Bilirubin (mg/dL)', 'Iron (mcg/dL)', 'Oxygen Saturation (%)', 'Ferritin (ng/mL)', 'Class']
+    target = 'Class'
+    df = pd.read_csv(input_file,    # Nome do arquivo com dados
+                     names=names)  # Nome das colunas
+    ShowInformationDataFrame(df, "Dataframe original")
+
+    # Separating out the features
+    x = df.loc[:, features].values
+
+    # Separating out the target
+    y = df.loc[:, [target]].values
+
+    # Standardizing the features
+    x = StandardScaler().fit_transform(x)
+    normalizedDf = pd.DataFrame(data=x, columns=features)
+    normalizedDf = pd.concat([normalizedDf, df[[target]]], axis=1)
+    ShowInformationDataFrame(normalizedDf, "Dataframe Normalized")
+
+    # PCA projection
+    pca = PCA()
+    principalComponents = pca.fit_transform(x)
+    print("Explained variance per component:")
+    print(pca.explained_variance_ratio_.tolist())
+    print("\n\n")
+
+    principalDf = pd.DataFrame(data=principalComponents[:, 0:2],
+                               columns=['principal component 1',
+                                        'principal component 2'])
+    finalDf = pd.concat([principalDf, df[[target]]], axis=1)
+    ShowInformationDataFrame(finalDf, "Dataframe PCA")
+
+    VisualizePcaProjection(finalDf, target)
+
+
+def ShowInformationDataFrame(df, message=""):
+    print(message+"\n")
+    print(df.info())
+    print(df.describe())
+    print(df.head(10))
+    print("\n")
+
+
+def VisualizePcaProjection(finalDf, targetColumn):
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_xlabel('Principal Component 1', fontsize=15)
+    ax.set_ylabel('Principal Component 2', fontsize=15)
+    ax.set_title('2 component PCA', fontsize=20)
+    targets = [0, 1, ]
+    colors = ['r', 'g']
+    for target, color in zip(targets, colors):
+        indicesToKeep = finalDf[targetColumn] == target
+        ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1'],
+                   finalDf.loc[indicesToKeep, 'principal component 2'],
+                   c=color, s=50)
+    ax.legend(targets)
+    ax.grid()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
