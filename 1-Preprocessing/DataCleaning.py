3f7fca2d3ed8e241a50b4e9d30276bb90ca871ee
Silas Leme Silvério <comikpro22@gmail.com>
2021-04-07 12:40:27 -0300

    Atualização do código com plot do histograma

--- 1-Preprocessing/DataCleaning.py
+++ 1-Preprocessing/DataCleaning.py
@@ -1,67 +1,76 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 def main():
     # Faz a leitura do arquivo
-    names = ['Gender', 'Symptoms', 'Alcohol', 'Hepatitis B Surface Antigen', 'Hepatitis B e Antigen', 'Hepatitis B Core Antibody', 'Hepatitis C Virus Antibody', 'Cirrhosis', 'Endemic Countries', 'Smoking', 'Diabetes', 'Obesity', 'Hemochromatosis', 'Arterial Hypertension', 'Chronic Renal Insufficiency', 'Human Immunodeficiency Virus', 'Nonalcoholic Steatohepatitis', 'Esophageal Varices', 'Splenomegaly', 'Portal Hypertension', 'Portal Vein Thrombosis', 'Liver Metastasis', 'Radiological Hallmark', 'Age at diagnosis', 'Grams of Alcohol per day', 'Packs of cigarets per year', 'Performance Status', 'Encefalopathy degree', 'Ascites degree', 'International Normalised Ratio', 'Alpha-Fetoprotein (ng/mL)', 'Haemoglobin (g/dL)', 'Mean Corpuscular Volume (fl)', 'Leukocytes(G/L)', 'Platelets (G/L)', 'Albumin (mg/dL)', 'Total Bilirubin(mg/dL)', 'Alanine transaminase (U/L)', 'Aspartate transaminase (U/L)', 'Gamma glutamyl transferase (U/L)', 'Alkaline phosphatase (U/L)', 'Total Proteins (g/dL)', 'Creatinine (mg/dL)', 'Number of Nodules', 'Major dimension of nodule (cm)', 'Direct Bilirubin (mg/dL)', 'Iron (mcg/dL)', 'Oxygen Saturation (%)', 'Ferritin (ng/mL)','Class'] 
-    features = ['Gender', 'Symptoms', 'Alcohol', 'Hepatitis B Surface Antigen', 'Hepatitis B e Antigen', 'Hepatitis B Core Antibody', 'Hepatitis C Virus Antibody', 'Cirrhosis', 'Endemic Countries', 'Smoking', 'Diabetes', 'Obesity', 'Hemochromatosis', 'Arterial Hypertension', 'Chronic Renal Insufficiency', 'Human Immunodeficiency Virus', 'Nonalcoholic Steatohepatitis', 'Esophageal Varices', 'Splenomegaly', 'Portal Hypertension', 'Portal Vein Thrombosis', 'Liver Metastasis', 'Radiological Hallmark', 'Age at diagnosis', 'Grams of Alcohol per day', 'Packs of cigarets per year', 'Performance Status', 'Encefalopathy degree', 'Ascites degree', 'International Normalised Ratio', 'Alpha-Fetoprotein (ng/mL)', 'Haemoglobin (g/dL)', 'Mean Corpuscular Volume (fl)', 'Leukocytes(G/L)', 'Platelets (G/L)', 'Albumin (mg/dL)', 'Total Bilirubin(mg/dL)', 'Alanine transaminase (U/L)', 'Aspartate transaminase (U/L)', 'Gamma glutamyl transferase (U/L)', 'Alkaline phosphatase (U/L)', 'Total Proteins (g/dL)', 'Creatinine (mg/dL)', 'Number of Nodules', 'Major dimension of nodule (cm)', 'Direct Bilirubin (mg/dL)', 'Iron (mcg/dL)', 'Oxygen Saturation (%)', 'Ferritin (ng/mL)','Class']
+    names = ['Gender', 'Symptoms', 'Alcohol', 'Hepatitis B Surface Antigen', 'Hepatitis B e Antigen', 'Hepatitis B Core Antibody', 'Hepatitis C Virus Antibody', 'Cirrhosis', 'Endemic Countries', 'Smoking', 'Diabetes', 'Obesity', 'Hemochromatosis', 'Arterial Hypertension', 'Chronic Renal Insufficiency', 'Human Immunodeficiency Virus', 'Nonalcoholic Steatohepatitis', 'Esophageal Varices', 'Splenomegaly', 'Portal Hypertension', 'Portal Vein Thrombosis', 'Liver Metastasis', 'Radiological Hallmark', 'Age at diagnosis', 'Grams of Alcohol per day', 'Packs of cigarets per year',
+             'Performance Status', 'Encefalopathy degree', 'Ascites degree', 'International Normalised Ratio', 'Alpha-Fetoprotein (ng/mL)', 'Haemoglobin (g/dL)', 'Mean Corpuscular Volume (fl)', 'Leukocytes(G/L)', 'Platelets (G/L)', 'Albumin (mg/dL)', 'Total Bilirubin(mg/dL)', 'Alanine transaminase (U/L)', 'Aspartate transaminase (U/L)', 'Gamma glutamyl transferase (U/L)', 'Alkaline phosphatase (U/L)', 'Total Proteins (g/dL)', 'Creatinine (mg/dL)', 'Number of Nodules', 'Major dimension of nodule (cm)', 'Direct Bilirubin (mg/dL)', 'Iron (mcg/dL)', 'Oxygen Saturation (%)', 'Ferritin (ng/mL)', 'Class']
+    features = ['Gender', 'Symptoms', 'Alcohol', 'Hepatitis B Surface Antigen', 'Hepatitis B e Antigen', 'Hepatitis B Core Antibody', 'Hepatitis C Virus Antibody', 'Cirrhosis', 'Endemic Countries', 'Smoking', 'Diabetes', 'Obesity', 'Hemochromatosis', 'Arterial Hypertension', 'Chronic Renal Insufficiency', 'Human Immunodeficiency Virus', 'Nonalcoholic Steatohepatitis', 'Esophageal Varices', 'Splenomegaly', 'Portal Hypertension', 'Portal Vein Thrombosis', 'Liver Metastasis', 'Radiological Hallmark', 'Age at diagnosis', 'Grams of Alcohol per day', 'Packs of cigarets per year',
+                'Performance Status', 'Encefalopathy degree', 'Ascites degree', 'International Normalised Ratio', 'Alpha-Fetoprotein (ng/mL)', 'Haemoglobin (g/dL)', 'Mean Corpuscular Volume (fl)', 'Leukocytes(G/L)', 'Platelets (G/L)', 'Albumin (mg/dL)', 'Total Bilirubin(mg/dL)', 'Alanine transaminase (U/L)', 'Aspartate transaminase (U/L)', 'Gamma glutamyl transferase (U/L)', 'Alkaline phosphatase (U/L)', 'Total Proteins (g/dL)', 'Creatinine (mg/dL)', 'Number of Nodules', 'Major dimension of nodule (cm)', 'Direct Bilirubin (mg/dL)', 'Iron (mcg/dL)', 'Oxygen Saturation (%)', 'Ferritin (ng/mL)', 'Class']
     output_file = '0-Datasets/hcc-dataClear.txt'
-    input_file = '0-Datasets/hcc-data.txt' 
-    df = pd.read_csv(input_file, # Nome do arquivo com dados
-                     names = names, # Nome das colunas 
-                     usecols = features, # Define as colunas que serão  utilizadas
-                     na_values='?') # Define que ? será considerado valores ausentes
-    
+    input_file = '0-Datasets/hcc-data.txt'
+    df = pd.read_csv(input_file,  # Nome do arquivo com dados
+                     names=names,  # Nome das colunas
+                     usecols=features,  # Define as colunas que serão  utilizadas
+                     na_values='?')  # Define que ? será considerado valores ausentes
+
     df_original = df.copy()
     # Imprime as 15 primeiras linhas do arquivo
     print("PRIMEIRAS 15 LINHAS\n")
     print(df.head(15))
-    print("\n")        
+    print("\n")
 
     # Imprime informações sobre dos dados
     print("INFORMAÇÕES GERAIS DOS DADOS\n")
     print(df.info())
     print("\n")
-    
+
     # Imprime uma analise descritiva sobre dos dados
     print("DESCRIÇÃO DOS DADOS\n")
     print(df.describe())
     print("\n")
-    
+
     # Imprime a quantidade de valores faltantes por coluna
     print("VALORES FALTANTES\n")
     print(df.isnull().sum())
     print("\n")
-    
+
     # Tratando valores faltantes da coluna Symptoms
     print("VALORES FALTANTES DA COLUNA Symptoms\n")
     print('Total valores ausentes: ' + str(df['Symptoms'].isnull().sum()))
 
-    
     columns_missing_value = df.columns[df.isnull().any()]
     print(columns_missing_value)
-    method = 'mode' # number or median or mean or mode
-    
+    method = 'mode'  # number or median or mean or mode
+
     for c in columns_missing_value:
         UptateMissingvalue(df, c)
-    
+
     print('Total valores ausentes: ' + str(df['Symptoms'].isnull().sum()))
     print(df.describe())
     print("\n")
     print(df.head(15))
     print(df_original.head(15))
     print("\n")
-    
+
     # Salva arquivo com o tratamento para dados faltantes
-    df.to_csv(output_file, header=False, index=False)  
-    
+    df.to_csv(output_file, header=False, index=False)
+
+    plt.title('Idade do grupo', fontsize=20)
+    plt.xlabel('Idade', fontsize=15)
+    plt.ylabel('Quantidade de pessoas', fontsize=15)
+    plt.hist(df['Age at diagnosis'], 5, rwidth=0.9, edgecolor='black')
+    plt.show()
+
 
 def UptateMissingvalue(df, column, method="mode", number=0):
     if method == 'number':
         # Substituindo valores ausentes por um número
         df[column].fillna(number, inplace=True)
     elif method == 'median':
-        # Substituindo valores ausentes pela mediana 
+        # Substituindo valores ausentes pela mediana
         median = df['Symptoms'].median()
         df[column].fillna(median, inplace=True)
     elif method == 'mean':
