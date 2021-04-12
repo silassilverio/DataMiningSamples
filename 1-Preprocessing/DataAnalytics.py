28a4da9c8db0e3265884c74f63c7874641989fcf
Silas Leme Silvério <comikpro22@gmail.com>
2021-04-12 16:33:31 -0300

    Analise de dados e Plot

--- 1-Preprocessing/DataAnalytics.py
+++ 1-Preprocessing/DataAnalytics.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -8,72 +9,50 @@ def main():
              'Performance Status', 'Encefalopathy degree', 'Ascites degree', 'International Normalised Ratio', 'Alpha-Fetoprotein (ng/mL)', 'Haemoglobin (g/dL)', 'Mean Corpuscular Volume (fl)', 'Leukocytes(G/L)', 'Platelets (G/L)', 'Albumin (mg/dL)', 'Total Bilirubin(mg/dL)', 'Alanine transaminase (U/L)', 'Aspartate transaminase (U/L)', 'Gamma glutamyl transferase (U/L)', 'Alkaline phosphatase (U/L)', 'Total Proteins (g/dL)', 'Creatinine (mg/dL)', 'Number of Nodules', 'Major dimension of nodule (cm)', 'Direct Bilirubin (mg/dL)', 'Iron (mcg/dL)', 'Oxygen Saturation (%)', 'Ferritin (ng/mL)', 'Class']
     features = ['Gender', 'Symptoms', 'Alcohol', 'Hepatitis B Surface Antigen', 'Hepatitis B e Antigen', 'Hepatitis B Core Antibody', 'Hepatitis C Virus Antibody', 'Cirrhosis', 'Endemic Countries', 'Smoking', 'Diabetes', 'Obesity', 'Hemochromatosis', 'Arterial Hypertension', 'Chronic Renal Insufficiency', 'Human Immunodeficiency Virus', 'Nonalcoholic Steatohepatitis', 'Esophageal Varices', 'Splenomegaly', 'Portal Hypertension', 'Portal Vein Thrombosis', 'Liver Metastasis', 'Radiological Hallmark', 'Age at diagnosis', 'Grams of Alcohol per day', 'Packs of cigarets per year',
                 'Performance Status', 'Encefalopathy degree', 'Ascites degree', 'International Normalised Ratio', 'Alpha-Fetoprotein (ng/mL)', 'Haemoglobin (g/dL)', 'Mean Corpuscular Volume (fl)', 'Leukocytes(G/L)', 'Platelets (G/L)', 'Albumin (mg/dL)', 'Total Bilirubin(mg/dL)', 'Alanine transaminase (U/L)', 'Aspartate transaminase (U/L)', 'Gamma glutamyl transferase (U/L)', 'Alkaline phosphatase (U/L)', 'Total Proteins (g/dL)', 'Creatinine (mg/dL)', 'Number of Nodules', 'Major dimension of nodule (cm)', 'Direct Bilirubin (mg/dL)', 'Iron (mcg/dL)', 'Oxygen Saturation (%)', 'Ferritin (ng/mL)', 'Class']
-    output_file = '0-Datasets/hcc-dataClear.txt'
-    input_file = '0-Datasets/hcc-data.txt'
+    input_file = '0-Datasets/hcc-dataClear.txt'
     df = pd.read_csv(input_file,  # Nome do arquivo com dados
                      names=names,  # Nome das colunas
                      usecols=features,  # Define as colunas que serão  utilizadas
                      na_values='?')  # Define que ? será considerado valores ausentes
 
     df_original = df.copy()
-    # Imprime as 15 primeiras linhas do arquivo
-    print("PRIMEIRAS 15 LINHAS\n")
-    print(df.head(15))
+
+    print("MÉDIA")
+    print(df.mean())
+    print("\n")
+    print("MODA")
+    print(df.mode())
+    print("\n")
+    print("MEDIANA")
+    print(df.median())
+    print("\n")
+    print("AMPLITUDE")
+    print(df.max() - df.min())
+    print("\n")
+    print("VARIÂNCIA")
+    print(df.var())
+    print("\n")
+    print("DESVIO PADRÃO")
+    print(df.std())
+    print("\n")
+    print("COVARIÂNCIA")
+    print(df.cov())
+    print("\n")
+    print("CORRELAÇÃO")
+    print(df.corr())
     print("\n")
 
-    # Imprime informações sobre dos dados
-    print("INFORMAÇÕES GERAIS DOS DADOS\n")
-    print(df.info())
-    print("\n")
+    plt.title('Idade do grupo', fontsize=20)
+    plt.xlabel('Idade', fontsize=15)
+    plt.ylabel('Quantidade de pessoas', fontsize=15)
+    plt.hist(df['Age at diagnosis'], 5, rwidth=0.9, edgecolor='black')
+    plt.show()
 
-    # Imprime uma analise descritiva sobre dos dados
-    print("DESCRIÇÃO DOS DADOS\n")
-    print(df.describe())
-    print("\n")
-
-    # Imprime a quantidade de valores faltantes por coluna
-    print("VALORES FALTANTES\n")
-    print(df.isnull().sum())
-    print("\n")
-
-    # Tratando valores faltantes da coluna Symptoms
-    print("VALORES FALTANTES DA COLUNA Symptoms\n")
-    print('Total valores ausentes: ' + str(df['Symptoms'].isnull().sum()))
-
-    columns_missing_value = df.columns[df.isnull().any()]
-    print(columns_missing_value)
-    method = 'mode'  # number or median or mean or mode
-
-    for c in columns_missing_value:
-        UptateMissingvalue(df, c)
-
-    print('Total valores ausentes: ' + str(df['Symptoms'].isnull().sum()))
-    print(df.describe())
-    print("\n")
-    print(df.head(15))
-    print(df_original.head(15))
-    print("\n")
-
-    # Salva arquivo com o tratamento para dados faltantes
-    df.to_csv(output_file, header=False, index=False)
-
-
-def UptateMissingvalue(df, column, method="mode", number=0):
-    if method == 'number':
-        # Substituindo valores ausentes por um número
-        df[column].fillna(number, inplace=True)
-    elif method == 'median':
-        # Substituindo valores ausentes pela mediana
-        median = df['Symptoms'].median()
-        df[column].fillna(median, inplace=True)
-    elif method == 'mean':
-        # Substituindo valores ausentes pela média
-        mean = df[column].mean()
-        df[column].fillna(mean, inplace=True)
-    elif method == 'mode':
-        # Substituindo valores ausentes pela moda
-        mode = df[column].mode()[0]
-        df[column].fillna(mode, inplace=True)
+    plt.title('Idade do grupo', fontsize=20)
+    plt.ylabel('Idade', fontsize=15)
+    plt.xlabel('Quantidade de pessoas', fontsize=15)
+    plt.plot(df['Age at diagnosis'])
+    plt.show()
 
 
 if __name__ == "__main__":
