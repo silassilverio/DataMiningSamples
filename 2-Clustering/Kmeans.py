6dcdf98a8e6d0c22cba54b18e759cf1177b5a5d3
Silas Leme Silvério <comikpro22@gmail.com>
2021-06-16 16:42:00 -0300

    Atualização clustering e Classification

--- 2-Clustering/Kmeans.py
+++ 2-Clustering/Kmeans.py
@@ -1,13 +1,14 @@
 # Implementation of Kmeans from scratch and using sklearn
 # Loading the required modules
+import pandas as pd
 import numpy as np
 from scipy.spatial.distance import cdist
-from sklearn.datasets import load_digits
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from sklearn.metrics import silhouette_samples
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 
 # Defining our kmeans function from scratch
 
@@ -40,23 +41,7 @@ def KMeans_scratch(x, k, no_of_iterations):
     return points
 
 
-def show_digitsdataset(digits):
-    fig = plt.figure(figsize=(6, 6))  # figure size in inches
-    fig.subplots_adjust(left=0, right=1, bottom=0,
-                        top=1, hspace=0.05, wspace=0.05)
-
-    for i in range(64):
-        ax = fig.add_subplot(8, 8, i + 1, xticks=[], yticks=[])
-        ax.imshow(digits.images[i], cmap=plt.cm.binary,
-                  interpolation='nearest')
-        # label the image with the target value
-        ax.text(0, 7, str(digits.target[i]))
-
-    # fig.show()
-
-
 def plot_samples(projected, labels, title):
-    fig = plt.figure()
     u_labels = np.unique(labels)
     for i in u_labels:
         plt.scatter(projected[labels == i, 0], projected[labels == i, 1], label=i,
@@ -67,31 +52,52 @@ def plot_samples(projected, labels, title):
     plt.title(title)
 
 
+def ShowInformationDataFrame(df, message=""):
+    print(message+"\n")
+    print(df.info())
+    print(df.describe())
+    print(df.head(10))
+    print("\n")
+
+
 def main():
     # Load dataset Digits
-    digits = load_digits()
-    show_digitsdataset(digits)
+    input_file = '0-Datasets/hcc-dataClear.txt'
+    names = ['Gender', 'Symptoms', 'Alcohol', 'Hepatitis B Surface Antigen', 'Hepatitis B e Antigen', 'Hepatitis B Core Antibody', 'Hepatitis C Virus Antibody', 'Cirrhosis', 'Endemic Countries', 'Smoking', 'Diabetes', 'Obesity', 'Hemochromatosis', 'Arterial Hypertension', 'Chronic Renal Insufficiency', 'Human Immunodeficiency Virus', 'Nonalcoholic Steatohepatitis', 'Esophageal Varices', 'Splenomegaly', 'Portal Hypertension', 'Portal Vein Thrombosis', 'Liver Metastasis', 'Radiological Hallmark', 'Age at diagnosis', 'Grams of Alcohol per day', 'Packs of cigarets per year',
+             'Performance Status', 'Encefalopathy degree', 'Ascites degree', 'International Normalised Ratio', 'Alpha-Fetoprotein (ng/mL)', 'Haemoglobin (g/dL)', 'Mean Corpuscular Volume (fl)', 'Leukocytes(G/L)', 'Platelets (G/L)', 'Albumin (mg/dL)', 'Total Bilirubin(mg/dL)', 'Alanine transaminase (U/L)', 'Aspartate transaminase (U/L)', 'Gamma glutamyl transferase (U/L)', 'Alkaline phosphatase (U/L)', 'Total Proteins (g/dL)', 'Creatinine (mg/dL)', 'Number of Nodules', 'Major dimension of nodule (cm)', 'Direct Bilirubin (mg/dL)', 'Iron (mcg/dL)', 'Oxygen Saturation (%)', 'Ferritin (ng/mL)', 'Class']
+    features = ['Age at diagnosis', 'Grams of Alcohol per day', 'Packs of cigarets per year',
+                'Performance Status', 'Encefalopathy degree', 'Ascites degree', 'International Normalised Ratio', 'Alpha-Fetoprotein (ng/mL)', 'Haemoglobin (g/dL)', 'Mean Corpuscular Volume (fl)', 'Leukocytes(G/L)', 'Platelets (G/L)', 'Albumin (mg/dL)', 'Total Bilirubin(mg/dL)', 'Alanine transaminase (U/L)', 'Aspartate transaminase (U/L)', 'Gamma glutamyl transferase (U/L)', 'Alkaline phosphatase (U/L)', 'Total Proteins (g/dL)', 'Creatinine (mg/dL)', 'Number of Nodules', 'Major dimension of nodule (cm)', 'Direct Bilirubin (mg/dL)', 'Iron (mcg/dL)', 'Oxygen Saturation (%)', 'Ferritin (ng/mL)', 'Class']
+    target = 'Class'
+    df = pd.read_csv(input_file,    # Nome do arquivo com dados
+                     names=names)  # Nome das colunas
+    k = 2
+    x = df.loc[:, features].values
+    y = df.loc[:, [target]].values
+
+    x = MinMaxScaler().fit_transform(x)
+    normalized2Df = pd.DataFrame(data=x, columns=features)
+    normalized2Df = pd.concat([normalized2Df, df[[target]]], axis=1)
+    ShowInformationDataFrame(normalized2Df, "Dataframe Min-Max Normalized")
 
     # Transform the data using PCA
-    pca = PCA(2)
-    projected = pca.fit_transform(digits.data)
-    print(pca.explained_variance_ratio_)
-    print(digits.data.shape)
-    print(projected.shape)
-    plot_samples(projected, digits.target, 'Original Labels')
+    pca = PCA()
+    projected = pca.fit_transform(x)
+    print("Explained variance per component:")
+    print(pca.explained_variance_ratio_.tolist())
+    print("\n\n")
 
     # Applying our kmeans function from scratch
-    labels = KMeans_scratch(projected, 10, 100)
+    labels = KMeans_scratch(projected, k, 100)
 
     # Visualize the results
     plot_samples(projected, labels, 'Clusters Labels KMeans from scratch')
 
     # Applying sklearn kemans function
-    kmeans = KMeans(n_clusters=10, random_state=0).fit(projected)
+    kmeans = KMeans(n_clusters=k, random_state=0).fit(projected)
     print(kmeans.inertia_)
     centers = kmeans.cluster_centers_
     score = silhouette_score(projected, kmeans.labels_)
-    print("For n_clusters = {}, silhouette score is {})".format(10, score))
+    print("For n_clusters = {}, silhouette score is {})".format(k, score))
 
     # Visualize the results sklearn
     plot_samples(projected, kmeans.labels_,
--- 3-Classification/SVM.py
+++ 3-Classification/SVM.py
@@ -5,59 +5,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import confusion_matrix
-from sklearn.neighbors import KNeighborsClassifier
-from collections import Counter
-
-# Calculate distance between two points
-
-
-def minkowski_distance(a, b, p=1):
-    # Store the number of dimensions
-    dim = len(a)
-    # Set initial distance to 0
-    distance = 0
-
-    # Calculate minkowski distance using parameter p
-    for d in range(dim):
-        distance += abs(a[d] - b[d])**p
-
-    distance = distance**(1/p)
-    return distance
-
-
-def knn_predict(X_train, X_test, y_train, y_test, k, p):
-    # Make predictions on the test data
-    # Need output of 1 prediction per test data point
-    y_hat_test = []
-
-    for test_point in X_test:
-        distances = []
-
-        for train_point in X_train:
-            distance = minkowski_distance(test_point, train_point, p=p)
-            distances.append(distance)
-
-        # Store distances in a dataframe
-        df_dists = pd.DataFrame(data=distances, columns=['dist'],
-                                index=y_train.index)
-
-        # Sort distances, and only consider the k closest points
-        df_nn = df_dists.sort_values(by=['dist'], axis=0)[:k]
-
-        # Create counter object to track the labels of k closest neighbors
-        counter = Counter(y_train[df_nn.index])
-
-        # Get most common label of all the nearest neighbors
-        prediction = counter.most_common()[0][0]
-
-        # Append prediction to output list
-        y_hat_test.append(prediction)
-
-    return y_hat_test
+from sklearn import datasets
+from sklearn.svm import SVC
 
 
 def plot_confusion_matrix(cm, classes,
@@ -82,6 +34,7 @@ def plot_confusion_matrix(cm, classes,
     else:
         print('Confusion matrix, without normalization')
 
+    cm = np.round(cm, 2)
     print(cm)
 
     thresh = cm.max() / 2.
@@ -95,12 +48,30 @@ def plot_confusion_matrix(cm, classes,
     plt.xlabel('Predicted label')
 
 
+def load_dataset(dataset='cancer'):
+    if dataset == 'iris':
+        # Load iris data and store in dataframe
+        iris = datasets.load_iris()
+        names = iris.target_names
+        df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+        df['target'] = iris.target
+    elif dataset == 'cancer':
+        # Load cancer data and store in dataframe
+        cancer = datasets.load_breast_cancer()
+        names = cancer.target_names
+        df = pd.DataFrame(data=cancer.data, columns=cancer.feature_names)
+        df['target'] = cancer.target
+
+    print(df.head())
+    return names, df
+
+
 def main():
-    # Load iris data and store in dataframe
+    # load dataset
     names = ['Gender', 'Symptoms', 'Alcohol', 'Hepatitis B Surface Antigen', 'Hepatitis B e Antigen', 'Hepatitis B Core Antibody', 'Hepatitis C Virus Antibody', 'Cirrhosis', 'Endemic Countries', 'Smoking', 'Diabetes', 'Obesity', 'Hemochromatosis', 'Arterial Hypertension', 'Chronic Renal Insufficiency', 'Human Immunodeficiency Virus', 'Nonalcoholic Steatohepatitis', 'Esophageal Varices', 'Splenomegaly', 'Portal Hypertension', 'Portal Vein Thrombosis', 'Liver Metastasis', 'Radiological Hallmark', 'Age at diagnosis', 'Grams of Alcohol per day', 'Packs of cigarets per year',
              'Performance Status', 'Encefalopathy degree', 'Ascites degree', 'International Normalised Ratio', 'Alpha-Fetoprotein (ng/mL)', 'Haemoglobin (g/dL)', 'Mean Corpuscular Volume (fl)', 'Leukocytes(G/L)', 'Platelets (G/L)', 'Albumin (mg/dL)', 'Total Bilirubin(mg/dL)', 'Alanine transaminase (U/L)', 'Aspartate transaminase (U/L)', 'Gamma glutamyl transferase (U/L)', 'Alkaline phosphatase (U/L)', 'Total Proteins (g/dL)', 'Creatinine (mg/dL)', 'Number of Nodules', 'Major dimension of nodule (cm)', 'Direct Bilirubin (mg/dL)', 'Iron (mcg/dL)', 'Oxygen Saturation (%)', 'Ferritin (ng/mL)', 'Class']  # Nome das colunas
     # Define as colunas que serão  utilizadas
-    features = ['Grams of Alcohol per day', 'Class']
+    features = ['Alcohol', 'Class']
     input_file = '0-Datasets/hcc-dataClear.txt'
     df = pd.read_csv(input_file,    # Nome do arquivo com dados
                      usecols=features,
@@ -124,32 +95,32 @@ def main():
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
 
-    # scaler = MinMaxScaler()
-    # X_train = scaler.fit_transform(X_train)
-    # X_test = scaler.transform(X_test)
-
-    # STEP 1 - TESTS USING knn classifier write from scratch
-    # Make predictions on test dataset using knn classifier
-    y_hat_test = knn_predict(X_train, X_test, y_train, y_test, k=5, p=2)
+    # TESTS USING SVM classifier from sk-learn
+    svm = SVC(kernel='poly')  # poly, rbf, linear
+    # training using train dataset
+    svm.fit(X_train, y_train)
+    # get support vectors
+    print(svm.support_vectors_)
+    # get indices of support vectors
+    print(svm.support_)
+    # get number of support vectors for each class
+    print("Qtd Support vectors: ")
+    print(svm.n_support_)
+    # predict using test dataset
+    y_hat_test = svm.predict(X_test)
 
     # Get test accuracy score
     accuracy = accuracy_score(y_test, y_hat_test)*100
     f1 = f1_score(y_test, y_hat_test, average='macro')
-    print("Acurracy K-NN from scratch: {:.2f}%".format(accuracy))
-    print("F1 Score K-NN from scratch: {:.2f}%".format(f1))
+    print("Acurracy SVM from sk-learn: {:.2f}%".format(accuracy))
+    print("F1 Score SVM from sk-learn: {:.2f}%".format(f1))
 
     # Get test confusion matrix
     cm = confusion_matrix(y_test, y_hat_test)
     plot_confusion_matrix(cm, ['Vive', 'Morre'], False,
-                          "Confusion Matrix - K-NN")
+                          "Confusion Matrix - SVM sklearn")
     plot_confusion_matrix(cm, ['Vive', 'Morre'], True,
-                          "Confusion Matrix - K-NN normalized")
-
-    # STEP 2 - TESTS USING knn classifier from sk-learn
-    knn = KNeighborsClassifier(n_neighbors=5)
-    knn.fit(X_train, y_train)
-    y_hat_test = knn.predict(X_test)
-
+                          "Confusion Matrix - SVM sklearn normalized")
     plt.show()
 
 
